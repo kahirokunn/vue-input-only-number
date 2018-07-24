852771b6fc801bcc6616f583646f91cbd7847e6d
k-okina <okinakahiro@gmail.com>
2018-07-24 20:26:14 +0900

    [add] バグ修正

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-param-reassign */
+
 const onlyFloat = {
   install(Vue, options = null) {
-    const directiveName = typeof options === 'object' && 'name' in options ?
+    const directiveName = options && typeof options === 'object' && 'name' in options ?
       options.name : 'float';
 
-    Vue.directive('float', {
+    Vue.directive(directiveName, {
       inserted(el) {
         el.onkeypress = (event) => {
           const approvalKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
@@ -18,10 +20,10 @@ const onlyFloat = {
 
 const onlyInt = {
   install(Vue, options = null) {
-    const directiveName = typeof options === 'object' && 'name' in options ?
+    const directiveName = options && typeof options === 'object' && 'name' in options ?
       options.name : 'int';
 
-    Vue.directive('int', {
+    Vue.directive(directiveName, {
       inserted(el) {
         el.onkeypress = (event) => {
           const approvalKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
