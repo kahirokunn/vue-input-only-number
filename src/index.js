ce70677795caa033b621748d1fd29456f43c64da
k-okina <okinakahiro@gmail.com>
2018-07-25 15:27:39 +0900

    [fix] 全角文字が入力できるバグを修正

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 /* eslint-disable no-param-reassign */
-
-const onlyFloat = {
-  install(Vue, options = null) {
-    const directiveName = options && typeof options === 'object' && 'name' in options ?
-      options.name : 'float';
-
-    Vue.directive(directiveName, {
-      inserted(el) {
-        el.onkeypress = (event) => {
-          const approvalKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-          if (!approvalKeys.includes(event.key)) {
-            event.preventDefault();
-          }
-        };
-      }
-    });
-  },
-};
-
 const onlyInt = {
   install(Vue, options = null) {
     const directiveName = options && typeof options === 'object' && 'name' in options ?
@@ -25,17 +6,13 @@ const onlyInt = {
 
     Vue.directive(directiveName, {
       inserted(el) {
-        el.onkeypress = (event) => {
-          const approvalKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-          if (!approvalKeys.includes(event.key)) {
-            event.preventDefault();
-          }
+        el.oninput = (event) => {
+          const formattedValue = parseInt(event.target.value, 10);
+          el.value = isNaN(formattedValue) ? '' : formattedValue;
         };
       }
     });
   },
 };
-
-export { onlyFloat };
 
 export default onlyInt;
